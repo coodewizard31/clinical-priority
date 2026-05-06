8c2c6c0b18e60388f6571f659253509baabb3e86
Frank <frankwamuyu40@gmail.com>
2026-05-06 04:39:08 +0300

    process of the user joining queue logic

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ const weekDays = [
     "Saturday",
     "Sunday",
 ];
+// 2. Main Logic - Runs whenever a page loads the process below is initiated 
+window.onload = function () {
+    // CHECK IF WE ARE ON THE BOOKING PAGE
+    const params = new URLSearchParams(window.location.search);
+    const department = params.get('dept');
+
+if (department) {
+    handleBookingPage(department);
+}
+
+// CHECK IF WE ARE ON THE HOME PAGE
+if (document.querySelector('.number')) {
+    updateHomeDisplay();
+}
+};
